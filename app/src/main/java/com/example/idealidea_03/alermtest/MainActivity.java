77bec13fd85454fae6c7bf1407d5225c9ac68a58
hyuni106 <suhyuni106@hanmail.net>
2018-01-09 16:12:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/idealidea_03/alermtest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.idealidea_03.alermtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
